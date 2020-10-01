11494b3de98dc474d8a5c8edcd0335ef2422a428
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2020-10-02 00:11:14 +0530

    console log used for debug

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 console.log("Hii!! JavaScript");
+
+// JS as Calculator
+console.log(34 + 34);
+console.log(23 * 4);
+console.log(90 - 67);
+console.log(67 / 2);
+
+// Console array
+console.log([23,56,45,43]);
+
+// console object
+console.log({name: "Shani Kumar Gupta", age: 23, course: "B.Tech"});
+
+//console object into table
+console.table({name: "Shani Kumar Gupta", age: 23, company: "Gemini Solutions Pvt. Ltd."});
+
+// console warning
+console.warn('This is my warning');
